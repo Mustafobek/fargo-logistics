9d561c0951c5c92c92334180dd6e139c37a75b32
Mustafobek <mustafobey99@mail.ru>
2022-03-23 17:04:28 +0300

    find and modify false

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,7 +6,8 @@ const DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/fargo-logistics'
 mongoose.connect(DB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 .then(r => console.log(infoLog('Connected to Database')))
 .catch(err => console.log(errorLog('Database connection error ' + err.message)))
