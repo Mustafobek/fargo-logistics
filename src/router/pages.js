5c2d1eff1fe5efb7b1c0c0e8bb3fce8a873bde3a
Mustafobek <mustafobey99@mail.ru>
2022-04-11 14:41:25 +0300

    statistics page

--- src/router/pages.js
+++ src/router/pages.js
@@ -9,6 +9,7 @@ r.get('/main', (req, res) => res.render('main.hbs', {title: 'Logistics'}))
 r.get('/create-order', (req, res) => res.render('create-order.hbs', {title: 'Create'}))
 r.get('/completed', (req, res) => res.render('completed.hbs', {title: 'Completeds'}))
 r.get('/order/:id', (req, res) => res.render('order.hbs', {title: 'Order'}))
+r.get('/statistics', (req, res) => res.render('statistics.hbs', {title: 'Order'}))
 
 
 // ADMIN
@@ -17,6 +18,7 @@ r.get('/ad/create-car-company', (req, res) => res.render('admin/create-car-compa
 r.get('/ad/create-car', (req, res) => res.render('admin/create-car.hbs', {title: 'Admin'}))
 r.get('/ad/create-user', (req, res) => res.render('admin/create-user.hbs', {title: 'Admin'}))
 r.get('/ad/create-route', (req, res) => res.render('admin/create-route.hbs', {title: 'Admin'}))
+r.get('/ad/statistics', (req, res) => res.render('admin/statistics.hbs', {title: 'Admin'}))
 
 
 // ERROR
